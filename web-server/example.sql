112e18175cad05aee2e4b0d3ed4f50f91e899e86
Chapman Wong Chak Man <chapmanwong604@gmail.com>
2022-07-11 03:57:56 +0800

    updating comment in example.sql

--- web-server/example.sql
+++ web-server/example.sql
@@ -2,5 +2,5 @@
 CREATE USER find_restaurant_user WITH PASSWORD '123456' SUPERUSER;
 CREATE DATABASE find_restaurant_db;
 
-/* Login to your database "foodie" with password "123456": */
+/* Login to your database "find_restaurant_db" with password "123456": */
 psql -U find_restaurant_user -W -h localhost find_restaurant_db;
